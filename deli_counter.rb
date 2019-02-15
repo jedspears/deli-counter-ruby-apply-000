49504fa9e06d74569f5d90d5267ce0d52bad1cf7
clever-genre-5225 <jed.spears7@gmail.com>
2019-02-15 14:02:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,27 +1,22 @@
 katz_deli = []
 counter = 0 
-
+serving = 1
 def line(deli)
-  if deli.empty?
-    line_str = "The line is currently empty."
+  if counter == 0
+    puts "The line is currently empty."
   else
-    line_str = "The line is currently: #{deli.length} customers long"
+    puts "There have been #{serving} customers today"
   end
-  puts line_str
 end 
 
 def take_a_number
   counter += 1 
   puts counter
-  
 end 
 
-def now_serving(deli)
-  if deli.empty?
-    puts "There is nobody waiting to be served!"
-  else 
-    puts "Currently serving #{deli.shift}."
-  end
+def now_serving
+  puts "Now serving: #{serving}"
+  serving += 1
 end 
 
 
